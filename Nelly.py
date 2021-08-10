0679db11b0b9fd4a44fb162ad4c8d62dbec78b79
daveh566 <69384676+daveh566@users.noreply.github.com>
2021-08-10 15:38:19 +0300

    Update Nelly.py

--- Nelly.py
+++ Nelly.py
@@ -1,3 +1,59 @@
+print("[INFO]: Importing Your API_ID, API_HASH, BOT_TOKEN")
+import re
+from asyncio import (gather, get_event_loop, sleep)
+
+from aiohttp import ClientSession
+from pyrogram import filters
+from Python_ARQ import ARQ
+
+from config import bot, BOT_TOKEN, ARQ_API_KEY, ARQ_API_BASE_URL, LANGUAGE
+bot_token= BOT_TOKEN
+
+print("[INFO]: Checking... Your Details")
+
+bot_id = int(bot_token.split(":")[0])
+print("[INFO]: Code running by master Prince Op")
+arq = None
+
+
+async def lunaQuery(query: str, user_id: int):
+    query = (
+        query
+        if LANGUAGE == "en"
+        else (await arq.translate(query, "en")).result.translatedText
+    )
+    resp = (await arq.luna(query, user_id)).result
+    return (
+        resp
+        if LANGUAGE == "en"
+        else (
+            await arq.translate(resp, LANGUAGE)
+        ).result.translatedText
+    )
+
+
+async def type_and_send(message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id if message.from_user else 0
+    query = message.text.strip()
+    await message._client.send_chat_action(chat_id, "typing")
+    response, _ = await gather(lunaQuery(query, user_id), sleep(2))
+    if "Luna" in response:
+        responsee = response.replace("Luna", "Nelly")
+    else:
+        responsee = response
+    if "Aco" in responsee:
+        responsess = responsee.replace("Aco", "Nelly")
+    else:
+        responsess = responsee
+    if "Who is Tiana?" in responsess:
+        responsess2 = responsess.replace("Who is Nelly?", "Heroine Of Telegram")
+    else:
+        responsess2 = responsess
+    await message.reply_text(responsess2)
+    await message._client.send_chat_action(chat_id, "cancel")
+
+
 @bot.on_message(
     filters.text & filters.reply & ~filters.bot & ~filters.via_bot & ~filters.forwarded,
     group=2,
