67ed92e7561b65b6297492914ff5409d36f6dec0
daveh566 <69384676+daveh566@users.noreply.github.com>
2021-08-10 16:03:51 +0300

    Update start.py

--- start.py
+++ start.py
@@ -6,9 +6,9 @@ Hello, I am Nelly [リュキア], An Intelligent ChatBot. If You Are Feeling Lon
 """
 
 
-@LYCIA.on_message(filters.command(["start"], prefixes = "/") & ~filters.edited)
+@bot.on_message(filters.command(["start"], prefixes = "/") & ~filters.edited)
 async def info(client, message):
     buttons = [
-                [InlineKeyboardButton("Lycia", switch_inline_query_current_chat="lycia "), InlineKeyboardButton("Github", url = "https://github.com/Red-Aura/Lyciachatbot")]
+                [InlineKeyboardButton("Bot", switch_inline_query_current_chat="bot "), InlineKeyboardButton("Add", url = "https://github.com/Red-Aura/Lyciachatbot")]
               ]
     await BOT.send_message(chat_id = message.chat.id, text = BOT_START, reply_markup = InlineKeyboardMarkup(buttons))
