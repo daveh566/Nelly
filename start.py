717c453de6015c90da969e2786cc5377115d7c26
daveh566 <69384676+daveh566@users.noreply.github.com>
2021-08-10 16:17:40 +0300

    Update start.py

--- start.py
+++ start.py
@@ -2,13 +2,15 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton
 
 BOT_START = """
-Hello, I am Nelly [リュキア], An Intelligent ChatBot. If You Are Feeling Lonely, You can Always Come to me and Chat With Me!
+Hello, I am Nelly , An Intelligent ChatBot by @KayAspirerProjecr. If You Are Feeling Lonely, You can Always Come to me and Chat With Me!
 """
 
 
 @bot.on_message(filters.command(["start"], prefixes = "/") & ~filters.edited)
 async def info(client, message):
-    buttons = [
-                [InlineKeyboardButton("Bot", switch_inline_query_current_chat="bot "), InlineKeyboardButton("Add", url = "https://github.com/Red-Aura/Lyciachatbot")]
-              ]
-    await BOT.send_message(chat_id = message.chat.id, text = BOT_START, reply_markup = InlineKeyboardMarkup(buttons))
+    )
+    buttons.add(
+        InlineKeyboardButton(
+            "👸🏼 Add Nelly to your group",
+            url=f"https://telegram.me/nellyvbot?startgroup=true",
+        await BOT.send_message(chat_id = message.chat.id, text = BOT_START, reply_markup = InlineKeyboardMarkup(buttons))
